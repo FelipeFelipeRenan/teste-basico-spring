f6030eb3eeff0c5f7b2fa49e8188d13eb58499f2
FelipeRenanRenan <feliperenanqwerty@gmail.com>
2022-01-19 22:41:34 -0300

    first commit

--- src/main/java/models/ProductModel.java
+++ src/main/java/models/ProductModel.java
@@ -1,5 +1,6 @@
 package models;
 
+import java.io.Serializable;
 import java.math.BigDecimal;
 
 import javax.persistence.Entity;
@@ -8,37 +9,38 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-import lombok.Data;
-
-
 @Entity
 @Table(name = "TB_PRODUTO")
-public class ProductModel {
-	
-	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	private long idProduto;
-	private String nome;
-	private BigDecimal valor;
-	
-	public long getIdProduto() {
-		return idProduto;
-	}
-	public void setIdProduto(long idProduto) {
-		this.idProduto = idProduto;
-	}
-	public String getNome() {
-		return nome;
-	}
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-	public BigDecimal getValor() {
-		return valor;
-	}
-	public void setValor(BigDecimal valor) {
-		this.valor = valor;
-	}
-	
+public class ProductModel implements Serializable{
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long idProduto;
+
+    private String nome;
+
+    private BigDecimal valor;
+
+    public ProductModel(String nome, BigDecimal valor) {
+        this.nome = nome;
+        this.valor = valor;
+    }
+    public long getIdProduto() {
+        return idProduto;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public BigDecimal getValor() {
+        return valor;
+    }
+    public void setValor(BigDecimal valor) {
+        this.valor = valor;
+    }
+
 
 }
