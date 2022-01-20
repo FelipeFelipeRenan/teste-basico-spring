229139485312f1b31a6cab2807705d5afbe7e648
FelipeRenanRenan <feliperenanqwerty@gmail.com>
2022-01-20 17:51:02 -0300

    Adding EntityScan annotation to fix the table creation on postgres

--- src/main/java/models/ProductModel.java
+++ src/main/java/models/ProductModel.java
@@ -9,9 +9,8 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-
-@Table(name = "TB_PRODUTO")
 @Entity
+@Table(name = "TB_PRODUTO")
 public class ProductModel implements Serializable{
 
     private static final long serialVersionUID = 1L;
