d14cea7bce5f3b1a66459251b6a578bbd4ae3ec8
FelipeRenanRenan <feliperenanqwerty@gmail.com>
2022-01-21 18:01:06 -0300

    Adding cliente controller

--- src/main/java/com/example/springboot/SpringbootApplication.java
+++ src/main/java/com/example/springboot/SpringbootApplication.java
@@ -2,9 +2,8 @@ package com.example.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 
-@EntityScan(basePackages = "com/example/springboot/models")
+
 @SpringBootApplication
 public class SpringbootApplication {
 
--- src/main/java/com/example/springboot/controllers/ClienteController.java
+++ src/main/java/com/example/springboot/controllers/ClienteController.java
@@ -1,4 +1,29 @@
 package com.example.springboot.controllers;
 
+import com.example.springboot.models.ClienteModel;
+import com.example.springboot.repositories.ClienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class ClienteController {
+
+    @Autowired
+    ClienteRepository clienteRepository;
+
+    @GetMapping("/clientes")
+    public ResponseEntity<List<ClienteModel>> getAllCliente(){
+        List<ClienteModel> clientesList = clienteRepository.findAll();
+        if(clientesList.isEmpty()){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        else{
+            return new ResponseEntity<List<ClienteModel>>(clientesList, HttpStatus.OK);
+        }
+    }
 }
